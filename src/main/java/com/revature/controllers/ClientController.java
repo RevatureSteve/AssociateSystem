f0986bc5849c44afce1eaf4eea7d6c2044942dc2
Dexter Hu <Dexter Hu>
2016-11-11 16:16:35 -0500

    A small commit for client controller to test

--- src/main/java/com/revature/controllers/ClientController.java
+++ src/main/java/com/revature/controllers/ClientController.java
@@ -1,8 +1,20 @@
 package com.revature.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.dto.AuthenticationDTO;
+import com.revature.pojos.User;
 
 @RestController
 public class ClientController {
 
+	@RequestMapping(value = "/authenticate")
+	public ResponseEntity<User> authenticate(AuthenticationDTO auth) {
+		User user = new User();
+		// TODO
+		return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
+	}
 }
