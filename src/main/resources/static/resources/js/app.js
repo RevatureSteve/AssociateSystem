fe63cf824138dab667b9887cdbb14042c7f920a2
Johnathan Cox <RyanC_Employment@hotmail.com>
2016-11-15 12:48:41 -0500

    Refactored pages in preparation for ngRoute.

--- src/main/resources/static/resources/js/app.js
+++ src/main/resources/static/resources/js/app.js
@@ -2,3 +2,13 @@ var app=angular.module("RAS",["ngRoute"]);
 
 app.controller("MainController",function($scope,$rootScope){
 });
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "login.html"
+    })
+    .when("/main", {
+        templateUrl : "main.html"
+    });
+});
