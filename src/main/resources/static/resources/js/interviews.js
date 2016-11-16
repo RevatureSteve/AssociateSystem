dd61af1a5a3e3a533ce2f2f76392648f8a052999
Johnathan Cox <RyanC_Employment@hotmail.com>
2016-11-16 16:35:17 -0500

    Formatted interview dummy data.

--- src/main/resources/static/resources/js/interviews.js
+++ src/main/resources/static/resources/js/interviews.js
@@ -14,6 +14,40 @@ app.controller("InterviewsPanel",function($scope,$rootScope,interviewDataService
 
 app.service("interviewDataService",function($http){
 	this.getAllInterviews=function(callback){
-		callback({data:[{client:"WalMart",info:"Dummy #"+Math.floor(Math.random()*100)},{client:"Tech Mahindra",info:"Dummy"}]});
+		callback({
+			data:[
+				{
+					interviewId:101,
+					job:
+					{
+						jobId:101,
+						account:
+						{
+							accountId:0,
+							accountName:"ITC"
+						},
+						client:
+						{
+							clientId:0,
+							clientName:"Walmart",
+							clientAvgStartDate:null,
+							clientAvgInterviewLength:null
+						},
+						status:
+						{
+							statusId:0,
+							statusName:""
+						},
+						jobContractLength:null,
+						jobTitle:"Jr. Java Developer"
+					},
+					user:{},
+					interviewTime:null,
+					interviewRound:1,
+					interviewType:"",
+					interviewFeedback:{}
+				}
+			]
+		});
 	};
 });
