02b0992df2acffe3c09f08825558506317febe02
jonmorals <jonmor0720@gmail.com>
2016-11-16 10:19:51 -0500

    making changes

--- src/test/java/com/revature/RepoApplicationTest.java
+++ src/test/java/com/revature/RepoApplicationTest.java
@@ -2,15 +2,24 @@ package com.revature;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.dao.AccountRepo;
+import com.revature.pojos.Account;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RepoApplicationTest {
 
+	@Autowired
+	AccountRepo accountRepo;
+	
 	@Test
-	public void contextLoads() {
+	public void makeAccountRepo() {
+		Account account = new Account("Walmart");
+		accountRepo.save(account);
 	}
 
 }
