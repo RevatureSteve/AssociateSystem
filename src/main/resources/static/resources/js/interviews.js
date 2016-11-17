c3b672a4ea80ba3f78253ffb96fa8edcd774c53a
Johnathan Cox <RyanC_Employment@hotmail.com>
2016-11-17 11:24:17 -0500

    Merged dummy tables into main page.

--- src/main/resources/static/resources/js/interviews.js
+++ src/main/resources/static/resources/js/interviews.js
@@ -4,71 +4,34 @@ app.controller("InterviewsPanel",function($scope,$rootScope,interviewDataService
 	$scope.getAllInterviews=function(){
 		return $scope.interviews;
 	}
+	$scope.getAllJobs=function(){
+		return $scope.jobs;
+	}
 	$scope.refreshAll=function(){
 		interviewDataService.getAllInterviews(function(response){
 			$scope.interviews=response.data;
+			setTimeout(function(){
+				var toBeRemoved=$("#dataTables_empty").parentElement;
+				if(toBeRemoved){
+					toBeRemoved.parentElement.remove(toBeRemoved);
+				}
+				$("#InterviewTable").DataTable();
+			},1000);
+		});
+		interviewDataService.getAllJobs(function(response){
+			$scope.jobs=response.data;
+			$("#dummyJobTable").DataTable();
 		});
 	};
 	$scope.refreshAll();
-	
-	// Dummy jobs/interviews!
-	// We will get some dummy data and show it in a table
-	$scope.dummyJobs = null;
-	$scope.dummyInterviews = null;
-	interviewDataService.getDummyJobs(function(response) {
-		$scope.dummyJobs = response.data;
-		$("#dummyJobTable").DataTable();
-	})
-	interviewDataService.getDummyInterviews(function(response) {
-		$scope.dummyInterviews = response.data;
-		$("#dummyInterviewTable").DataTable();
-	})
 });
 
 app.service("interviewDataService",function($http){
 	this.getAllInterviews=function(callback){
-		callback({
-			data:[
-				{
-					interviewId:101,
-					job:
-					{
-						jobId:101,
-						account:
-						{
-							accountId:0,
-							accountName:"ITC"
-						},
-						client:
-						{
-							clientId:0,
-							clientName:"Walmart",
-							clientAvgStartDate:null,
-							clientAvgInterviewLength:null
-						},
-						status:
-						{
-							statusId:0,
-							statusName:""
-						},
-						jobContractLength:null,
-						jobTitle:"Jr. Java Developer"
-					},
-					user:{},
-					interviewTime:null,
-					interviewRound:1,
-					interviewType:"",
-					interviewFeedback:{}
-				}
-			]
-		});
+		$http.get('dummyInterviews').then(callback);
 	};
 	
-	this.getDummyJobs = function(callback) {
-		$http.get('dummyJobs').then(callback);
-	}
-	
-	this.getDummyInterviews = function(callback) {
-		$http.get('dummyInterviews').then(callback);
+	this.getAllJobs = function(callback) {
+		$http.get('jobs').then(callback);
 	}
 });
