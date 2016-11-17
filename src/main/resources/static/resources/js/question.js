21e0f1498be7dd5f8cd276c10709f75d1d30053a
Dexter Hu <Dexter Hu>
2016-11-17 14:19:30 -0500

    question and navbar

---
+++ src/main/resources/static/resources/js/question.js
@@ -0,0 +1,9 @@
+var app=angular.module("RAS");
+
+app.controller("QuestionPanel",function($scope, $rootScope, questionDataService){
+
+});
+
+app.service("questionDataService", function($http, $rootScope) {
+	
+});
