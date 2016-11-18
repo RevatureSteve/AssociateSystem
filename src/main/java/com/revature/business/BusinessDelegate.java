c7497dd4064b1eac39a2abdd109eb3dd0c72a6a9
Dexter Hu <Dexter Hu>
2016-11-18 15:15:37 -0500

    added business delegate and services.  moved code to services.

---
+++ src/main/java/com/revature/business/BusinessDelegate.java
@@ -0,0 +1,25 @@
+package com.revature.business;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.revature.dto.AuthenticationDTO;
+import com.revature.dto.InterviewDTO;
+import com.revature.dto.JobDTO;
+import com.revature.pojos.Interview;
+import com.revature.pojos.TechQuestion;
+import com.revature.pojos.User;
+
+public interface BusinessDelegate {
+	
+	public ResponseEntity<List<InterviewDTO>> dummyInterviews();
+	public ResponseEntity<List<JobDTO>> dummyJobs();
+	public ResponseEntity<List<Interview>> getInterviewsFor(int userId);
+	
+	public ResponseEntity<User> authenticate(AuthenticationDTO auth);
+	
+	public List<TechQuestion> getQuestions();
+	public List<TechQuestion> getQuestionsByTrack(@RequestParam("catName") String catName);
+}
